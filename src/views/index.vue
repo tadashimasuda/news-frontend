50a8d2082e89e0323f4ba31d1b8fb96e31c827d6
tadashimasuda <tmasu4260@gmail.com>
2021-06-25 16:52:29 +0900

    add:category btn

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,14 +1,54 @@
 <template>
-  <div class="index">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <v-app>
+    <v-container class="mt-5">
+      <v-list-item row style="overflow-x:scroll">
+        <v-list-item >
+          <v-btn>
+          <span>tag</span>
+        </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn>
+          <span>tag</span>
+        </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn>
+          <span>tag</span>
+          </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn>
+          <span>tag</span>
+          </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn>
+          <span>tag</span>
+        </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn>
+          <span>tag</span>
+        </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn>
+          <span>tag</span>
+        </v-btn>
+        </v-list-item>
+      </v-list-item>
+      
+    </v-container>
+    
+  </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: 'index',
+  name: 'Index',
   components: {
   }
 }
