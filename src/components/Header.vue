4508724ca4102a883f267fbea63c53e8f7a822c8
tadashimasuda <tmasu4260@gmail.com>
2021-06-25 15:20:12 +0900

    update:header
    nav-icon,title, search icon, login btn

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,21 @@
 <template>
-    <header>
-        
-    </header>
+    <v-app-bar
+      absolute
+      color="white"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>News</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn depressed color="white">
+        <span>ログイン</span>
+      </v-btn>
+    </v-app-bar>
 </template>
 <script>
 
