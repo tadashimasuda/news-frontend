73cfd425c8a52246bbc8d9b3b8b35ff981677147
tadashimasuda <tmasu4260@gmail.com>
2021-06-25 21:13:49 +0900

    add:components Popup
    fix #4 #2 propでコンポーネントに特定の記事を受け取れるようにした

--- src/views/index.vue
+++ src/views/index.vue
@@ -38,28 +38,46 @@
         </v-btn>
         </v-list-item>
       </v-list-item>
-      <div v-masonry transition-duration="0.3s" item-selector=".item">
-        <v-card v-masonry-tile class="item mx-1 mt-2" :class="addClass(index)" :key=index v-for="(item, index) in Number(10)">
-            <v-img class="white--text align-end" src="https://i.imgur.com/kwxMKGr.jpg">
-              <v-card-title>Top 10 Australian beaches</v-card-title>
-            </v-img>
-            <v-card-subtitle class="pb-0">
-              Number 10
-            </v-card-subtitle>
-            <v-card-text class="text--primary">
-            </v-card-text>
-        </v-card>
-      </div>
+      <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <div v-masonry transition-duration="0.3s" item-selector=".item" v-bind="attrs" v-on="on"  >
+            <v-card v-masonry-tile class="item mx-1 mt-2" :class="addClass(index)" :key=index v-for="(item, index) in Number(10)" @click="articleData(index)">
+                <v-img class="white--text align-end" src="https://i.imgur.com/kwxMKGr.jpg">
+                  <v-card-title>Top 10 Australian beaches</v-card-title>
+                </v-img>
+                <v-card-subtitle class="pb-0">
+                  Number 
+                </v-card-subtitle>
+                <v-card-text class="text--primary">
+                </v-card-text>
+            </v-card>
+          </div>
+        </template>
+      <Popup :article=article @dialogUpdate="dialog=false" />
+      </v-dialog>
     </v-container>
   </v-app>
 </template>
 
 <script>
+import Popup from '@/components/Popup.vue'
 export default {
   name: 'Index',
   components: {
+    Popup
+  },
+  data () {
+      return {
+        dialog: false,
+        article:{
+          num:''
+        }
+      }
   },
   methods: {
+    articleData(index){
+      this.article.num=index
+    },
     addClass: function(){
       let min = 1 ;
       let max = 3 ;
