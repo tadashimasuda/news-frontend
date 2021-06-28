b4a3e7546dad0e30086c976d51fd9def086a7252
tadashimasuda <tmasu4260@gmail.com>
2021-06-28 21:34:20 +0900

    update:responsible対応

--- src/views/articles.vue
+++ src/views/articles.vue
@@ -3,7 +3,7 @@
         <v-content>
             <v-container>
                 <v-row justify="center" align-content="center">
-                    <v-col cols="10">
+                    <v-col md="10">
                         <v-card>
                             <div ></div>
                             <v-img
