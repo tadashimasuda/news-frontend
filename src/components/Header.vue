77ea17f5d0329af95e1f11cd554eb6d5254b4629
tadashimasuda <tmasu4260@gmail.com>
2021-07-02 21:01:53 +0900

    update:header avatar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,8 +9,64 @@
         <v-btn icon>
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
-        <v-btn v-if="!$store.state.UserStatus" depressed color="white" href="/oauth/twitter/redirect">ログイン</v-btn>
-        <v-btn v-if="$store.state.UserStatus" depressed color="white">ログアウト</v-btn>
+        <template v-if="!$store.state.userStatus">
+          <v-btn depressed color="white" href="/oauth/twitter/redirect">ログイン</v-btn>
+        </template>
+        <template v-else>
+          <v-menu
+            bottom
+            min-width="200px"
+            rounded
+            offset-y
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                x-large
+                v-on="on"
+              >
+                <v-avatar
+                  color="brown"
+                  size="43"
+                >
+                  <v-img
+                  height="100%"
+                  :src = $store.state.user.img_path
+                  size="100"
+                  mx-auto
+                  class="rounded-circle justify-center my-5"
+                >
+                </v-img>
+                </v-avatar>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-list-item-content class="justify-center">
+                <div class="mx-auto text-center">
+                  <v-avatar>
+                    <v-img
+                      height="100%"
+                      :src = $store.state.user.img_path
+                      size="100"
+                      mx-auto
+                      class="rounded-circle justify-center my-5"
+                    >
+                    </v-img>
+                  </v-avatar>
+                  <h4 class="mt-2">{{$store.state.user.name}}</h4>
+                  <v-divider class="my-3"></v-divider>
+                  <v-btn
+                    depressed
+                    rounded
+                    text
+                  >
+                    ログアウト
+                  </v-btn>
+                </div>
+              </v-list-item-content>
+            </v-card>
+          </v-menu>
+        </template>
       </v-app-bar>
       <v-navigation-drawer v-model="drawer" fixed temporary>
         <v-list nav dense>
